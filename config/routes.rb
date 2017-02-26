1d341d5cc2459ef367197de2909f81f35101a116
yukako yamaoka <y.yucco-de_tte_pc@outlook.jp>
2017-02-26 10:57:38 +0000

    create massages_controoller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  get 'messages/index'
+
+  root 'messages#index'
+ #, to: "messages#index"
+  
+  #get 'test' , to: "messages#test"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
